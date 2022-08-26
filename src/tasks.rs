f3e3c1d67b01c8de7b2ac28d0b0d0faf1ca02bfb
Misael Pereira <misamagna@gmail.com>
2022-08-26 15:23:25 +0200

    Adde add task function

--- src/tasks.rs
+++ src/tasks.rs
@@ -16,11 +16,29 @@ impl Task {
     }
 }
 
-use std::io::Result:
+use std::fs:{File, OpenOptions}
 use std::path::PathBuf;
+use std::io::{Result, Seek, SeekFrom};
 
 pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
+    let mut file: File = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(journal_path)?;
 
+    let mut tasks: Vec<Task> = match serde_json::from_reader(&file) {
+        Ok(tasks) => tasks,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?,
+    }
+
+    file.seek(SeekFrom::Start(0));
+
+    tasks.push(task);
+    serde_json::to_writer(file, &tasks)?;
+
+    Ok(())
 }
 
 pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
@@ -28,5 +46,5 @@ pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> 
 }
 
 pub fn list_tasks(journal_path_ PathBuf) -> Result<()> {
-    
+
 }
